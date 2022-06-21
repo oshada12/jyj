4e69cc4602a364fbfc400667fe749c73b6a04abe
oshada12 <107744144+oshada12@users.noreply.github.com>
2022-06-21 16:17:48 +0100

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,19 +1,17 @@
-const { mediafireDl } = require('../lib/mediafire.js')
-let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
-if (!args[0].includes('mediafire.com')) return m.reply(error.linkmf)
-let mdjon = args.join(' ')
-res = await mediafireDl(mdjon)
-result = `「 *MEDIAFIRE DOWNLOAD* 」
-*Data Berhasil Didapatkan!*
-🆔 Nama : ${res[0].nama}
-📊 Ukuran : ${res[0].size}
-💬 Link : ${res[0].link}
-_Tunggu Proses Upload Media_`
-m.reply(result)
-//await sleep(100)
-conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
-}
-handler.command = ['mediafire']
+import { lookup } from 'mime-types'
+import { mediafiredl } from '@bochilteam/scraper'
 
-module.exports = handler
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw 'Input URL' 
+	if (!/https?:\/\/(www\.)?mediafire\.com/.test(args[0])) throw 'Invalid URL' 
+	let res = await mediafiredl(args[0])
+	let mimetype = await lookup(res.url)
+	delete res.url2
+	m.reply(Object.keys(res).map(v => `*• ${v.capitalize()}:* ${res[v]}`).join('\n') + '\n\n_Sending file..._')
+	conn.sendMessage(m.chat, { document: { url: res.url }, fileName: res.filename, mimetype }, { quoted: m })
+}
+handler.help = handler.alias = ['mediafire']
+handler.tags = ['downloader']
+handler.command = /^(mediafire)$/i
+
+export default handler
